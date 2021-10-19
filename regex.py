22d1b80f293ae2807591f0b526200dd0d228e62c
Megan Goldstein <EthanG@megans-air-3.attlocal.net>
2021-10-18 22:40:05 -0400

    completed three functions with regular expressions -- find_word, find_days, and find_domains

--- regex.py
+++ regex.py
@@ -8,7 +8,7 @@ def read_file(filename):
     # Open the file and get the file object
     source_dir = os.path.dirname(__file__) #<-- directory name
     full_path = os.path.join(source_dir, filename)
-    infile = open(full_path,'r', encoding='utf-8')
+    infile = open(full_path,'r', encoding = 'utf-8')
     
     # Read the lines from the file object into a list
     lines = infile.readlines()
@@ -23,55 +23,74 @@ def find_word(string_list):
     """ Return a list of words that contain three digit numbers in the middle. """
 
     # initialize an empty list
-
+    lst1 = []
+    blank_lst = []
     # define the regular expression
-
+    str1 = "[a-z]+\d{3}+[a-z]+"
     # loop through each line of the string list 
-
+    for line in string_list:
     # find all the words that match the regular expression in each line
-    
+        found_words = re.findall(str1, line)
+        blank_lst.append(found_words)
     # loop through the found words and add the words to your empty list 
-
+    for word in blank_lst:
     #return the list of all words that start with the letter B, E, or T
-    pass
-
+        if word[0] == 'B' or word[0] == 'E' or word[0] == 'T':
+            lst1.append(word)
+    return lst1
 
 def find_days(string_list):
     """ Return a list of days from the list of strings the dates format in the text are MM/DD/YYYY. """  
 
     # initialize an empty list
-
+    lst2 = []
+    blank_lst2 = []
     # define the regular expression
-
+    str2 = "[0-9]{1-2}.[0-9]{1-2}.[0-9]{4}"
     # loop through each line of the string list
-    
+    for line in string_list:
     # find all the dates that match the regular expression in each line
-    
+        found_dates = re.findall(str2, line)
+        blank_lst2.append(found_dates)
     # loop through the found dates and only add the days to your empty list 
-    
+    for date in blank_lst2:
+        day_str = "[0-9]{1-2}([0-9]{1-2})"
+        day = re.findall(day_str, date)
+        lst2.append(day)
     #return the list of days
-    pass
+    return lst2
 
 def find_domains(string_list):
     """ Return a list of web address domains from the list of strings the domains of a wbsite are after www. """
 
     # initialize an empty list
-
+    lst3 = []
+    extra_lst = []
+    blank_lst3 = []
     # define the regular expression
-
+    str3 = "http.+"
     # loop through each line of the string list
-
+    for line in string_list:
     # find all the domains that match the regular expression in each line
-
+        found_domains = re.findall(str3, line)
+        blank_lst3.append(found_domains)
     # loop through the found domains
-
+        for domain in blank_lst3:
+            str4 = "https?.{3}+(.+)"
     # get the domain name by splitting the (//) after the https or http to get the website name
+            www_name = re.findall(str4, domain)
+            extra_lst.append(www_name)
     # then strip the www. to get only the domain name
-
+        for www_name in extra_lst:
+            if "www." in www_name:
+                domain_name = www_name.strip("www.")
+            else:
+                domain_name = www_name
     # add the domains to your empty list
+            lst3.append(domain_name)
     
     #return the list of domains
-    pass
+    return lst3
 
 class TestAllMethods(unittest.TestCase):
 
